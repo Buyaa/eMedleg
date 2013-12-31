68eee64b4eaf32b4821901523e534b376a595cc8
oshko <oshko.mail@gmail.com>
2013-12-30 23:07:49 -0800

    Bower and angular route added for pages

--- src/main/webapp/lib/app.js
+++ src/main/webapp/lib/app.js
@@ -1,60 +1,16 @@
 var app = angular.module('myApp',  ['ngResource', 'ngRoute', 'ui.bootstrap'] );
 
 
-app.controller('topMenuCtrl', function($scope, topMenuResource, $modal, $http, userAuthSer, userLoginSer){
-	topMenuResource.query(function(menuData){
-		$scope.topMenuData = menuData;
-		console.log($scope.topMenuData);
-	});
 
-    $scope.user = {};
-    $scope.user.auth = userAuthSer.isLoggedIn;
-
-
-
-    if(userAuthSer.isLoggedIn == false){
-        $scope.user.btnTxt = "Нэвтрэх";
-        console.log($scope.user.btnTxt);
-
-        $scope.login = function(){
-            var modelInstance = $modal.open({
-                templateUrl: './lib/partials/loginPar.html',
-                controller: ModalInstanceCtrl
-            });
-        };
-
-        var ModalInstanceCtrl = function ($scope, $modalInstance) {
-
-
-            $scope.user = {};
-            $scope.user.email = "";
-            $scope.user.password = "";
-            $scope.user.alert = "";
-            var modalInstance = $modalInstance;
-
-            $scope.userLogin = function(){
-                userLoginSer.userCheck($scope.user, modalInstance);
-            };
-
-            $scope.cancel = function () {
-                modalInstance.dismiss('cancel');
-            };
-        };
-    } else {
-        $scope.user.btnTxt = "Гарах";
-        console.log($scope.user.btnTxt);
-    };
-
-
+app.config(function($routeProvider){
+    $routeProvider.when('/',
+        {
+            templateUrl: './lib/partials/newsPar.html',
+            controller: 'latestNewsCtrl'
+        });
+    $routeProvider.when('/addnews',
+        {
+            templateUrl: './lib/partials/addNewsPar.html'
+        });
 
 });
-
-
-
-app.controller('latestNewsCtrl', function($scope, latestContent){
-	latestContent.query(function(newsData){
-		$scope.latestNewsData = newsData;
-		console.log($scope.latestNewsData);
-	});
-});
-
---
+++ src/main/webapp/lib/controllers/mainCtrls.js
@@ -0,0 +1,61 @@
+/**
+ * Created by oshko on 12/30/13.
+ */
+
+app.controller('topMenuCtrl', function($scope, topMenuResource, $modal, $http, userAuthSer, userLoginSer){
+    topMenuResource.query(function(menuData){
+        $scope.topMenuData = menuData;
+        console.log($scope.topMenuData);
+    });
+
+    $scope.user = {};
+    $scope.user.auth = userAuthSer.isLoggedIn;
+
+
+
+    if(userAuthSer.isLoggedIn == false){
+        $scope.user.btnTxt = "Нэвтрэх";
+        console.log($scope.user.btnTxt);
+
+        $scope.login = function(){
+            var modelInstance = $modal.open({
+                templateUrl: './lib/partials/loginPar.html',
+                controller: ModalInstanceCtrl
+            });
+        };
+
+        var ModalInstanceCtrl = function ($scope, $modalInstance) {
+
+
+            $scope.user = {};
+            $scope.user.email = "";
+            $scope.user.password = "";
+            $scope.user.alert = "";
+            var modalInstance = $modalInstance;
+
+            $scope.userLogin = function(){
+                userLoginSer.userCheck($scope.user, modalInstance);
+            };
+
+            $scope.cancel = function () {
+                modalInstance.dismiss('cancel');
+            };
+        };
+    } else {
+        $scope.user.btnTxt = "Гарах";
+        console.log($scope.user.btnTxt);
+    };
+
+
+
+});
+
+
+
+app.controller('latestNewsCtrl', function($scope, latestContent){
+    latestContent.query(function(newsData){
+        $scope.latestNewsData = newsData;
+        console.log($scope.latestNewsData);
+    });
+});
+
